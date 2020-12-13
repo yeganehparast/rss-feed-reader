30cda569ce77c1cfe08e29131a38a1d5c3f5da4b
Mehdi Yeganehparast <mehdi.yeganehparast@gmail.com>
2020-12-13 15:36:03 +0100

    Initial Commit

---
+++ src/main/java/com/assingment/feed/RssFeedReaderApplication.java
@@ -0,0 +1,13 @@
+package com.assingment.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assingment/feed/RssFeedReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assingment.feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssFeedReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
